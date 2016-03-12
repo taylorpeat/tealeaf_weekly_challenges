6feca9b9fedc77bcf838e184317d1e4f80709344
Taylor Peat <taylorpeat@hotmail.com>
2016-03-12 06:21:53 -0500

    Trinary Challenge

--- trinary.rb
+++ trinary.rb
@@ -2,7 +2,6 @@ class Trinary
   attr_reader :to_decimal
 
   def initialize(tri_num)
-    tri_num = tri_num.chars.map(&:to_i)
-    @to_decimal = tri_num.inject(0) { |sum, num| (sum + num) * 3 } / 3
+    @to_decimal = tri_num.chars.map(&:to_i).inject(0) { |sum, num| (sum + num) * 3 } / 3
   end
 end
