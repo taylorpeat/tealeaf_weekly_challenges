e43c3f5d792b6c1d4bbd20109d17eb687e27fce4
Taylor Peat <taylorpeat@hotmail.com>
2016-03-12 06:27:20 -0500

    add if statement

--- trinary.rb
+++ trinary.rb
@@ -1,7 +1,8 @@
 class Trinary
   attr_reader :to_decimal
 
-  def initialize(tri_num)
-    @to_decimal = tri_num.chars.map(&:to_i).inject(0) { |sum, num| (sum + num) * 3 } / 3
+  def initialize(tri_str)
+    tri_num = tri_str.to_i == 0 ? [0] : tri_str.chars.map(&:to_i) 
+    @to_decimal = tri_num.inject(0) { |sum, num| (sum + num) * 3 } / 3
   end
 end
