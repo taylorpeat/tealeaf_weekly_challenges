95823d863efe845e87d3abeb0aba71a0f1db224e
Taylor Peat <taylorpeat@hotmail.com>
2016-03-26 02:28:04 -0400

    Refactor saddle points

--- saddle_points.rb
+++ saddle_points.rb
@@ -1,37 +1,14 @@
-require 'pry'
 class Matrix
-  attr_reader :rows
+  attr_reader :rows, :columns, :saddle_points
 
   def initialize(matrix_string)
     @rows = matrix_string.split("\n").map { |str| str.scan(/\d+/).map(&:to_i) } 
+    @columns = rows.transpose
+    coordinates = Array(0..rows.size - 1).product(Array(0..rows.first.size - 1))
+    @saddle_points = coordinates.select { |co| check_coord(co) }
   end
 
-  def columns
-    temp_columns = []
-    rows.size.times do |num|
-      temp_columns << rows.map { |row| row[num] }.flatten
-    end
-    temp_columns
-  end
-
-  def saddle_points
-    saddle_points = []
-    rows.each_with_index do |row, row_num|
-      columns.each_with_index do |col, col_num|
-        saddle_points << [row_num, col_num] if max?(row, col_num) && min?(col, row_num)
-      end
-    end
-    saddle_points
-  end
-
-  def max?(array, idx)
-    array[idx] == array.max
-  end
-
-  def min?(array, idx)
-    array[idx] == array.min
+  def check_coord(co)
+    rows[co[0]][co[1]] == rows[co[0]].max && rows[co[0]][co[1]] == columns[co[1]].min
   end
 end
-
-
-
