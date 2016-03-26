dd97651a0218b1387b0ecb5411fd8a10152b639e
Taylor Peat <taylorpeat@hotmail.com>
2016-03-26 02:32:24 -0400

    Refactor saddle points

--- saddle_points.rb
+++ saddle_points.rb
@@ -5,10 +5,10 @@ class Matrix
     @rows = matrix_string.split("\n").map { |str| str.scan(/\d+/).map(&:to_i) } 
     @columns = rows.transpose
     coordinates = Array(0..rows.size - 1).product(Array(0..rows.first.size - 1))
-    @saddle_points = coordinates.select { |co| check_coord(co) }
+    @saddle_points = coordinates.select { |coord| check_for_saddle(coord) }
   end
 
-  def check_coord(co)
+  def check_for_saddle(co)
     rows[co[0]][co[1]] == rows[co[0]].max && rows[co[0]][co[1]] == columns[co[1]].min
   end
 end
