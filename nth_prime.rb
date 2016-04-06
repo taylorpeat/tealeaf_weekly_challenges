579df267af931549047de94facb8966e7ec7e4cf
Taylor Peat <taylorpeat@hotmail.com>
2016-04-05 22:58:00 -0400

    update nth prime

--- nth_prime.rb
+++ nth_prime.rb
@@ -2,7 +2,8 @@ class Prime
   def self.nth(target)
     raise ArgumentError if target < 1
     (2..1.0/0).each_with_object([]) do |num, primes|
-      primes << num unless primes.any? { |prime| num % prime == 0 }
+      sqrt = num**0.5
+      primes << num unless primes.any? { |p| p <= sqrt ? num % p == 0 : break }
       return primes.pop if primes.length == target
     end
   end
