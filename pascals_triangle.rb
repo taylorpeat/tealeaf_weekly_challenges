823b9f1ce73476e2091cf47aa2f303ba2d94be9f
Taylor Peat <taylorpeat@hotmail.com>
2016-04-29 11:06:14 -0400

    back to original

--- pascals_triangle.rb
+++ pascals_triangle.rb
@@ -7,12 +7,10 @@ class Triangle
 
   def create_triangle(num_of_rows)
     (1..(num_of_rows - 1)).each_with_object(Array.new(num_of_rows, [1])) do |row_num, rows|
-      current_row = rows[row_num]
-      prev_row = rows[row_num - 1]
       (row_num - 1).times do |i|
-        current_row += [prev_row[i] + prev_row[i + 1]]
+        rows[row_num] += [rows[row_num - 1][i] + rows[row_num - 1][i + 1]]
       end
-      current_row += [1]
+      rows[row_num] += [1]
     end
   end
 end
