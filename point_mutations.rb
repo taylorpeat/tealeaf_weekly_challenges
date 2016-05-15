fe305e738026e72afd1c50ac39f6ed0595c8a881
Taylor Peat <taylorpeat@hotmail.com>
2016-05-15 03:56:14 -0400

    point mutation challenge

--- point_mutations.rb
+++ point_mutations.rb
@@ -6,13 +6,12 @@ class DNA
   end
 
   def hamming_distance(dna2)
-    distance = 0
     length = dna1.length < dna2.length ? dna1.length : dna2.length
-    for i in 0..(length - 1)
+    (0..(length - 1)).reduce(0) do |distance, i|
       if dna1[i] != dna2[i]
         distance += 1
       end
+      distance
     end
-    distance
   end
 end
