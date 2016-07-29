00255212f52fd2ca9ca101b4cfd6a647d0a45884
Taylor Peat <taylorpeat@hotmail.com>
2016-07-28 23:30:06 -0500

    Linked list start

--- bowling.rb
+++ bowling.rb
@@ -1,64 +1,72 @@
 class Game
-  ROUNDS = 10
+  FRAMES = 10
 
-  attr_reader :rounds
+  attr_reader :frames
   
   def initialize
-    @rounds = []
+    @frames = []
   end
 
   def roll(score)
     check_valid_roll(score)
     
-    rounds.last&.status == :open ? rounds.last.add(score) : rounds << Round.new(score)
+    if current_frame_open?
+      frames.last.add(score)
+    else
+      frames << Frame.new(score)
+    end
 
-    rounds.slice(0, rounds.length - 1).each do |round|
-      round.add(score) if round.status != :finished
+    frames.slice(0, frames.length - 1).each do |frame|
+      frame.add(score) if frame.status != :closed
     end
   end
 
   def score
-    raise unfinished_game_error_message unless game_finished?
-    rounds.slice(0, ROUNDS).reduce(0) { |total, round| total + round.total }
+    raise unclosed_game_error_message unless game_closed?
+    frames.slice(0, FRAMES).reduce(0) { |total, frame| total + frame.total }
   end
 
   def check_valid_roll(score)
     raise 'Pins must have a value from 0 to 10' unless (0..10).cover?(score)
-    raise 'Should not be able to roll after game is over' if game_finished?
+    raise 'Should not be able to roll after game is over' if game_closed?
   end
 
-  def game_finished?
-    rounds[ROUNDS - 1]&.status == :finished
+  def current_frame_open?
+    frames.last&.status == :current_frame 
   end
 
-  def unfinished_game_error_message
-    return 'Game is not yet over, cannot score!' if @rounds[ROUNDS - 1]&.status == :xtra_balls
+  def game_closed?
+    frames[FRAMES - 1]&.status == :closed
+  end
+
+  def unclosed_game_error_message
+    return 'Game is not yet over, cannot score!' if @frames[FRAMES - 1]&.status == :bonus
     'Score cannot be taken until the end of the game'
   end
 end
 
-class Round
-  PIN_TOTAL = 10
+class Frame
+  SPARE_OR_STRIKE_SCORE = 10
   attr_accessor :status
   attr_reader :scores
 
   def initialize(initial_score)
     @scores = [initial_score]
-    @status = initial_score == PIN_TOTAL ? :xtra_balls : :open
+    @status = initial_score == SPARE_OR_STRIKE_SCORE ? :bonus : :current_frame
   end
 
   def add(score)
     scores.push(score)
-    raise 'Pin count exceeds pins on the lane' if total > 10 && status == :open
-    self.status = :xtra_balls if total == PIN_TOTAL
-    self.status = :finished if round_finished?
+    raise 'Pin count exceeds pins on the lane' if total > 10 && status == :current_frame
+    self.status = :bonus if total == SPARE_OR_STRIKE_SCORE
+    self.status = :closed if frame_closed?
   end
 
   def total
     scores.reduce(:+)
   end
 
-  def round_finished?
+  def frame_closed?
     scores.length == 3 || total < PIN_TOTAL && scores.length == 2
   end
 end
